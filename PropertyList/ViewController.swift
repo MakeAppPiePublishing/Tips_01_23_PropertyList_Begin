ec7d901e0f45965655d38507d5c193a2c155840f
Steve Lipton <stevelipton@brk-16892.local>
2018-04-26 06:18:03 -0700

    initial Commit

--- PropertyList/ViewController.swift
+++ PropertyList/ViewController.swift
@@ -2,24 +2,26 @@
 //  ViewController.swift
 //  PropertyList
 //
-//  Created by Steve Lipton on 4/26/18.
-//  Copyright © 2018 Steve Lipton. All rights reserved.
+//  Created by Steven Lipton on 4/24/18.
+//  Copyright © 2018 Steven Lipton. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var backgroundImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
 
